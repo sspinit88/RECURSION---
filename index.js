74e5d6dfde604b2a81d0ae7d9bd0e576c142d4bc
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-02-03 20:55:44 +0400

    1 file modified

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 /**
- * Что бы вычислить минимальное значение, необходимо заменить 
+ * Что бы вычислить минимальное значение, необходимо заменить
  * Math.max на Math.min
-*/
-const max = (array, i = 0) => {
-    /**
-     * Если в массиве есть только одно значение,
-     * то можем вернуть его в качестве ответа.
-    */
-    if (array.length - 1 === 0) {
-      return array[0];
-    }
-  
-    const firstValue = array[i];
-    const secondValue = max(array.splice(1), i++);
+ */
+const max = (array) => {
+  /**
+   * Если в массиве есть только одно значение,
+   * то можем вернуть его в качестве ответа.
+   */
+  if (array.length - 1 === 0) {
+    return array[0];
+  }
 
-    return Math.max(firstValue, secondValue);
-  };
-  
-  const test = [15, -78, 1, 8, 10, 2, 88, 16];
-  
-  console.log(max(test)); /// 88
+  let i = 0;
+
+  if (array > 0) {
+    i++;
+  }
+
+  const firstValue = array[i];
+  const secondValue = max(array.splice(1));
+
+  return Math.max(firstValue, secondValue);
+};
+
+const test = [15, -78, 1, 8, 10, 2, 88, 16];
+
+console.log(max(test)); /// 88
