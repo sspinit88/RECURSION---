3a08cb17d49616b18c65b832c910250f4e3f50e0
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-02-03 20:44:02 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RECURSION---
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-psg57p)
---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Что бы вычислить минимальное значение, необходимо заменить 
+ * Math.max на Math.min
+*/
+const max = (array, i, length) => {
+  /**
+   * Если в массиве есть, только одно значение,
+   * то можем вернуть его в качестве ответа
+  */
+  if (length === 1) {
+    return array[0];
+  }
+
+  return Math.min(array[i], max(array, i += 1, length - 1));
+};
+
+const test = [15, -78, 1, 8, 10, 2, 88, 16];
+
+console.log(max(test, 0, test.length)); /// 88
+
