46ae644398bcda3b1ba339efa702c08171c12d86
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-02-03 21:04:02 +0400

    1 file modified

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ const max = (array) => {
   const i = 0;
   const firstValue = array[i];
   const secondValue = max(array.splice(1));
-
+  /**
+   * будем сравнивать первое число массива, со следующим,
+   * каждый раз сдвигаясь вправо.
+  */
   return Math.max(firstValue, secondValue);
 };
 
 const test = [15, -78, 258, 1, 8, 10, 2, 88, 16, 100];
 
-console.log(max(test)); /// 88
+console.log(max(test)); /// 258
