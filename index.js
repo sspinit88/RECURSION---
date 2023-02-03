384b72243c2f62d658330bc724d77477129cb1a2
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-02-03 20:50:53 +0400

    1 file modified

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
  * Что бы вычислить минимальное значение, необходимо заменить 
  * Math.max на Math.min
 */
-const max = (array, i, length) => {
-  /**
-   * Если в массиве есть только одно значение,
-   * то можем вернуть его в качестве ответа
-  */
-  if (length === 1) {
-    return array[0];
-  }
+const max = (array, i = 0) => {
+    /**
+     * Если в массиве есть только одно значение,
+     * то можем вернуть его в качестве ответа
+    */
+    if (array.length === 1) {
+      return array[0];
+    }
+  
+    const firstValue = array[i];
+    const secondValue = max(array.splice(1), i++);
 
-  return Math.max(array[i], max(array, i += 1, length - 1));
-};
-
-const test = [15, -78, 1, 8, 10, 2, 88, 16];
-
-console.log(max(test, 0, test.length)); /// 88
-
+    return Math.max(firstValue, secondValue);
+  };
+  
+  const test = [15, -78, 1, 8, 10, 2, 88, 16];
+  
+  console.log(max(test)); /// 88
